0aff08d700dca7554e73dbf5be00f4218c824b1c
eromose-le <erons.a.gberaese@gmail.com>
2025-05-25 15:41:58 +0100

    chore: implement role and permission UI

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -15,3 +15,4 @@ export { default as Balances } from "assets/icons/balances.svg?react";
 export { default as Payouts } from "assets/icons/payouts.svg?react";
 export { default as Settlements } from "assets/icons/settlements.svg?react";
 export { default as VirtualWallets } from "assets/icons/virtual-wallets.svg?react";
+export { default as Rider } from "assets/icons/rider.svg?react";
