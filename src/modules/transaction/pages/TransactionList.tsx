694a3f3005fd11d0e81b907f1e3594bd64efa1eb
Joseph Edache <josedache0@gmail.com>
2025-05-29 14:35:45 +0100

    feat: transaction filter

--- src/modules/transaction/pages/TransactionList.tsx
+++ src/modules/transaction/pages/TransactionList.tsx
@@ -1,13 +1,22 @@
 import useTable from "hooks/use-table";
 import TransactionTable, { columns } from "../features/TransactionTable";
 import TransactionsTableHeader from "../features/TransactionTableHeader";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Paper } from "@mui/material";
 import { orderApi } from "apis/order.ts";
 import usePagination from "hooks/use-pagination.ts";
+import { TransactionFilterState } from "modules/transaction/features/TransactionFilter.tsx";
+import { useSnackbar } from "notistack";
+import * as dfns from "date-fns";
 
 const TransactionList = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [filter, setFilter] = useState(() => ({}) as TransactionFilterState);
+
   const [pagination, setPagination] = usePagination();
+
+  const [getOrdersQuery] = orderApi.useLazyGetOrdersQuery();
 
   const ordersQueryResult = orderApi.useGetOrdersQuery(
     useMemo(
@@ -15,9 +24,16 @@ const TransactionList = () => {
         params: {
           Page: pagination.pageIndex + 1,
           Limit: pagination.pageSize,
+          Status: filter.Status || undefined,
+          FromDate: filter.FromDate
+            ? dfns.format(filter.FromDate, "yyyy-MM-dd")
+            : undefined,
+          ToDate: filter.ToDate
+            ? dfns.format(filter.ToDate, "yyyy-MM-dd")
+            : undefined,
         },
       }),
-      [pagination.pageIndex, pagination.pageSize]
+      [pagination.pageIndex, pagination.pageSize, filter]
     )
   );
 
@@ -32,9 +48,34 @@ const TransactionList = () => {
     onPaginationChange: setPagination,
   });
 
+  async function handleExport() {
+    try {
+      const data = await getOrdersQuery({
+        params: {
+          Status: filter.Status || undefined,
+          FromDate: filter.FromDate || undefined,
+          ToDate: filter.ToDate || undefined,
+          isExport: true,
+        },
+      }).unwrap();
+      enqueueSnackbar(data?.message || "Transactions exported Successful", {
+        variant: "success",
+      });
+    } catch (error) {
+      enqueueSnackbar(error?.message || "Transaction Export Failed", {
+        variant: "error",
+      });
+    }
+  }
+
   return (
     <div className="space-y-6">
-      <TransactionsTableHeader total={orders?.page?.total} />
+      <TransactionsTableHeader
+        total={orders?.page?.total}
+        filter={filter}
+        onFilterApply={setFilter}
+        onExport={handleExport as any}
+      />
       <Paper>
         <TransactionTable
           instance={tableInstance}
